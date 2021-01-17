7789e140df0262037b7649f4aa47d07fb5d29ce9
pannu <ankitapannu1@gmail.com>
2021-01-17 14:37:15 -0800

    changed tweet events design a bit, added stats class

---
+++ src/main/Stats.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Stats {
+	
+	private int dayCount;
+	private int[] timeCount;
+	
+	public Stats () {
+		dayCount = 0;
+		timeCount = new int[1440];
+	}
+	
+	public void addEvent(int hour, int min) {
+		this.dayCount++;
+		int index = (hour * 60) + (min * 24);
+		this.timeCount[index] += 1; 
+	}
+	
+	public int getDayCount() {
+		return this.dayCount;
+	}
+	
+	public int[] getTimeCount() {
+		return this.timeCount;
+	}
+
+}
--- src/main/TweetEvents.java
+++ src/main/TweetEvents.java
@@ -1,11 +1,14 @@
 package main;
 
-import java.sql.Date;
+import java.util.Date;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import main.Stats;
 
 public class TweetEvents {
 	
@@ -13,52 +16,70 @@ public class TweetEvents {
 	private int count;
 	
 	
-	private TreeMap<TweetEvents, TreeMap<int[], int[]>> tweets;
-	private TreeMap<TweetEvents, TreeMap<int[], int[]>> retweets;
-	private TreeMap<TweetEvents, TreeMap<int[], int[]>> likes;
-	private TreeMap<TweetEvents, TreeMap<int[], int[]>> replies;
+	private TreeMap<Long, Stats> tweets;
+	private TreeMap<Long, Stats> retweets;
+	private TreeMap<Long, Stats> likes;
+	private TreeMap<Long, Stats> replies;
+	private ArrayList<TreeMap<Long, Stats>> logs;
 	
-	public TweetEvents(String t, int c) {
-		this.timestamp = t;
-		this.count = c;
+	public TweetEvents() {
+		tweets = new TreeMap<Long, Stats>();
+		retweets = new TreeMap<Long, Stats>();
+		likes = new TreeMap<Long, Stats>();
+		replies = new TreeMap<Long, Stats>();
+		logs = new ArrayList<TreeMap<Long, Stats>>();
+		logs.add(tweets);
+		logs.add(retweets);
+		logs.add(likes);
+		logs.add(replies);
 	}
 	
-	public String getTimestamp() {
-		return this.timestamp;
-	}
-	
-	public int getCount() {
-		return this.count;
+	public TreeMap<Long, Stats> getTweets() {
+		return this.tweets;
 	}
 	
 	public void recordEntry(String eventType, long timestamp) {
 		Date d = new Date(timestamp);
 		int year = d.getYear() + 1900;
+		System.out.println("year: " + year);
 		int month = d.getMonth() + 1;
+		System.out.println("month: " + month);
 		int date = d.getDate();
-		String fullDate = year + "-" + month + "-" + date;
+		System.out.println("date: " + date);
+		long yearLong = year * 1000L;
+		long monthLong = month * 100L;
+		long dateLong = date * 10L;
+		long fullDate = yearLong + monthLong + dateLong;
 		
-		int hours = d.getHours();
-		int minutes = d.getMinutes();
+		int hour = d.getHours();
+		System.out.println("hour: " + hour);
+		int min = d.getMinutes();
+		System.out.println("min: " + min);
 		
-		switch (eventType){
-			case "tweet":
-			case "retweet":
-			case "like":
-			case "reply":
-				
-			default:
-				throw new IllegalArgumentException("Invalid event type");
+		if (tweets.containsKey(fullDate)) {
+			Stats currentStats = tweets.get(fullDate);
+			currentStats.addEvent(hour, min);
+			tweets.put(fullDate, currentStats);
+		} else {
+			Stats newStats = new Stats();
+			newStats.addEvent(hour, min);
+			tweets.put(fullDate, newStats);
 		}
 		
 	}
 	
-	public HashMap<Long, Integer> getEventCounts(String eventType, long start, long end, String granularity) {
-		if (end <= start) {
-			throw new IllegalArgumentException("End timestamp must proceed start timestamp");
+	public static void main(String[] args) {
+		long l = 1610863602L;
+		TweetEvents t = new TweetEvents();
+		t.recordEntry("tweets", l);
+		TreeMap<Long, Stats> tweets = t.getTweets();
+		for (Map.Entry<Long, Stats> entry : tweets.entrySet()) {
+			System.out.println(entry.getKey());
+			Stats s = entry.getValue();
+			System.out.println(s.getDayCount());
+			int[] arr = s.getTimeCount();
+			System.out.println(arr[804]);
 		}
-		// TODO
-		return null;
 	}
 	
 
